38c1b9eef81d71f744570886314cf8fac764f70a
jpierresaldana <jp.saldana@outlook.es>
2022-09-13 22:23:03 -0500

    Routes n Controller de Restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,17 @@
+class RestaurantsController < ApplicationController
+  def index
+    # @restaurants = Restaurant.new
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+end
