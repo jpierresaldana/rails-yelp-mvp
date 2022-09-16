07e557997d290a12d2ac6566960fb975c8323409
jpierresaldana <jp.saldana@outlook.es>
2022-09-15 20:36:37 -0500

    Add review in restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,20 @@
 class ReviewsController < ApplicationController
-  def index
-
-  end
-
   def new
-
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
-
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurants_path(@restaurant)
   end
 
-  def show
-  
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: %i[index new create show] do
-    resources :reviews, only: %i[index new create show]
+    resources :reviews, only: %i[new create]
   end
 end
