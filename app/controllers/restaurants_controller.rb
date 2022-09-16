8d1d823af7e055b63abed22ece710e26abd4156e
jpierresaldana <jp.saldana@outlook.es>
2022-09-15 19:30:42 -0500

    Restaurant Show/index Rests, new/create a rest ok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,24 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.new
+    @restaurants = Restaurant.all
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-
+    @restaurant = Restaurant.create(params_restaurant)
+    redirect_to restaurants_path(@restaurant)
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
+  private
+
+  def params_restaurant
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,6 +12,6 @@ class ReviewsController < ApplicationController
   end
 
   def show
-
+  
   end
 end
